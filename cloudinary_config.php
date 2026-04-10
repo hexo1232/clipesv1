21adc6ca0dd26de1399a7593c65c1c8eef17af0e
Matias Matavel <hexodier1232@gmail.com>
2026-04-11 00:42:48 +0200

    22 commit

--- cloudinary_config.php
+++ cloudinary_config.php
@@ -1,8 +1,28 @@
 <?php
-require 'vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Cloudinary\Cloudinary;
 
-$cloudinary = new Cloudinary(
-    getenv('CLOUDINARY_URL')
-);
+// Busca a URL de ambiente
+$cloudinaryUrl = getenv('CLOUDINARY_URL');
+
+if (!$cloudinaryUrl) {
+    throw new \Exception("Erro: CLOUDINARY_URL não configurada no Render.");
+}
+
+/**
+ * LIMPEZA DE SEGURANÇA:
+ * Remove os caracteres < e > caso tenham sido colados por engano no painel do Render.
+ * Remove também espaços em branco.
+ */
+$urlLimpa = str_replace(['<', '>', ' '], '', $cloudinaryUrl);
+
+try {
+    // Retorna a instância configurada com a URL higienizada
+    return new Cloudinary([
+        'url' => $urlLimpa
+    ]);
+} catch (\Exception $e) {
+    // Se ainda assim der erro, mostra uma mensagem amigável
+    die("Falha na configuração do Cloudinary: " . $e->getMessage());
+}
--- config/cloudinary.php
+++ config/cloudinary.php
@@ -3,15 +3,26 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Cloudinary\Cloudinary;
 
+// Busca a URL de ambiente
 $cloudinaryUrl = getenv('CLOUDINARY_URL');
 
 if (!$cloudinaryUrl) {
-    throw new \Exception("Variável CLOUDINARY_URL não configurada no Render.");
+    throw new \Exception("Erro: CLOUDINARY_URL não configurada no Render.");
 }
 
-// Limpeza de segurança: remove espaços e os sinais < > caso você esqueça algum
-$urlLimpa = str_replace(['<', '> ', ' '], '', $cloudinaryUrl);
+/**
+ * LIMPEZA DE SEGURANÇA:
+ * Remove os caracteres < e > caso tenham sido colados por engano no painel do Render.
+ * Remove também espaços em branco.
+ */
+$urlLimpa = str_replace(['<', '>', ' '], '', $cloudinaryUrl);
 
-return new Cloudinary([
-    'url' => $urlLimpa
-]);
+try {
+    // Retorna a instância configurada com a URL higienizada
+    return new Cloudinary([
+        'url' => $urlLimpa
+    ]);
+} catch (\Exception $e) {
+    // Se ainda assim der erro, mostra uma mensagem amigável
+    die("Falha na configuração do Cloudinary: " . $e->getMessage());
+}
