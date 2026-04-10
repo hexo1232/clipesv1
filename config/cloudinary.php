7932e88691680530cfb358da51eea611c501173c
Matias Matavel <hexodier1232@gmail.com>
2026-04-11 00:33:03 +0200

    20 commit

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -1,8 +1,16 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
+use Cloudinary\Configuration\Configuration;
 use Cloudinary\Cloudinary;
 
+// Tenta pegar a URL da variável de ambiente
+$cloudinaryUrl = getenv('CLOUDINARY_URL');
+
+if (!$cloudinaryUrl) {
+    throw new \Exception("Erro: Variável CLOUDINARY_URL não encontrada no ambiente.");
+}
+
 return new Cloudinary([
-    'url' => getenv('CLOUDINARY_URL')
+    'url' => $cloudinaryUrl
 ]);
